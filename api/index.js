d2dddfcb809e9e553399d5f2fa93458726dc9a92
luuduc2727 <trumsnap@gmail.com>
2024-10-24 10:21:33 +0700

    add

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
+require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
 const path = require('path');
 const app = express();
-const mysql = require('mysql2');
 const mongoose = require('mongoose')
 const articlerouter = require("../routes/articles")
 const Article = require('./../models/article')
 const methodOverrride = require('method-override')
-
+const PORT = process.env.PORT || 3000
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/luuduc'
+const NODE_ENV = process.env.NODE_ENV
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +18,7 @@ app.use('/css', express.static('D:/BloggingWebAppClone/css'));
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverrride('_method'))
 
-mongoose.connect('mongodb://localhost/luuduc')
+mongoose.connect(MONGODB_URI)
 app.get('/', async(req, res) => {
 
     const article = await Article.find().sort({createAt: 'desc'})
@@ -29,7 +29,7 @@ app.get('/', async(req, res) => {
 app.use('/articles', articlerouter)
 
 // Chạy server
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on http://localhost:3000');
 });
 
